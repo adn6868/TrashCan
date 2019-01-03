4843c425d950eafc5a951b700630f6387b3239d9
Anh Nguyen_MacbookPro <anhnd32@fsoft.com.vn>
2019-01-03 14:17:49 -0600

    isPalidrome is complete with O(n) time and O(1) space

--- dataStruct/LinkedList.py
+++ dataStruct/LinkedList.py
@@ -4,17 +4,17 @@ class LinkedList:
 		# super(LinkedList, self).__i nit__()
 		self.val = x
 		self.next = None
-	def addLast(self,aNode):
+	def addLast(self,aNode): #O(n)
 		cur = self
 		while cur.next:
 			cur = cur.next
 		cur.next = aNode
-
-	def add(self,aNumb):
+ 
+	def add(self,aNumb): #O(n)
 		newNode = LinkedList(aNumb)
 		self.addLast(newNode)
 
-	def toString(self):
+	def toString(self): #O(n)
 		ans = ''
 		cur = self
 		while cur:
@@ -27,20 +27,40 @@ def main():
 	a.add(2)
 	a.add(3)
 	a.add(4)
-	a.add(5)
+	a.add(3)
+	a.add(2)
+	a.add(1)
 	print(a.toString())
-	reverse_in_place(a)
-def reverse_in_place(a):
+	print (isPalidrome(a))
+
+def reverse_in_place(a): #O(n)
 	cur = a
 	head = a
 	nex = None
 	prev = None
-	while cur:
+	while cur: 
 		nex = cur.next
 		cur.next = prev
 		prev = cur
 		cur = nex
 	a = prev
-	print(a.toString())
+	return a
+
+def isPalidrome(a): #O(n)
+	it1 = a
+	it2 = a
+	while it2.next: #O(n)
+		it1 = it1.next
+		it2 = it2.next.next
+	it2 = a
+	it1 = reverse_in_place(it1)
+	print(it1.toString())
+	print(it2.toString())
+	while it2:  #O(n)
+		if it2.val != it1.val:
+			return False
+		it2 = it2.next
+		it1 = it1.next
+	return True
 if __name__ == '__main__':
 	main()
