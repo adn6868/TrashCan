15e8fec2830fc7a90ee3322df57875e77ba71213
Anh Nguyen_MacbookPro <anhnd32@fsoft.com.vn>
2019-01-15 11:05:38 -0600

    get.py no concurrency

---
+++ Rest/get.py
@@ -0,0 +1,20 @@
+import requests
+import time
+
+
+def get(url, session):
+	with session.get(url) as response:
+		print("get %d from %s" % (len(response.content), url))
+
+def getList(sites):
+	with requests.Session() as session:
+		for url in sites:
+			get(url,session)
+
+if __name__ == '__main__':
+	print("Naive Single processing:")
+	sites = ['http://google.com', 'http://apple.com'] * 10
+	startTime = time.time()
+	getList(sites)
+	duration =time.time()-startTime
+	print("download %d in %s seconds " % (len(sites),str(duration)) )
