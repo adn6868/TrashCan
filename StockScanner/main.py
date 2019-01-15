a2e2268a37a252460288f187cc9da1b77a47aa7f
Anh Nguyen_MacbookPro <anhnd32@fsoft.com.vn>
2019-01-15 16:12:36 -0600

    clean up

--- StockScanner/main.py
+++ StockScanner/main.py
@@ -4,42 +4,38 @@ import threading
 import concurrent.futures
 import json
 
-class MyULF:
-	"""docstring for MyULF"""
+class UrlBuilder:
 	def __init__(self, key = 'EBBVJ2TH7C8Y5HEA'):
 		self.key = key
-		self.header = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey='
 		self.symbol = ''
+		self.function = ''
 		self.interval = ''
 		self.outputsize = ''
-		self.VALID_INTERVAL = ['1min','5min']
+		self.VALID_FUNCTIONS = ''
+		self.VALID_INTERVALS = ['1min','5min']
 
-	
 	def setSymbol(self, symbol):
 		self.symbol = symbol.upper()
-	def setInterval(self,interval = "1min"):
-		if interval in self.VALID_INTERVAL:
+	def setInterval(self,interval = "5min"):
+		if interval in self.VALID_INTERVALS:
 			self.interval = interval
 		elif not self.interval:
-			self.interval = self.VALID_INTERVAL[0]
+			self.interval = self.VALID_INTERVALS[0]
 	def setOutputSize(self,size = "full"):
 		self.outputsize = size
 	def getURL(self):
 		if self.symbol:
-			return  "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={0}&interval={1}&outputsize={2}&apikey={3}".format(self.symbol,self.interval,self.outputsize,self.key)
+			return  "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={0}&interval={1}&outputsize={2}&apikey={3}"\
+			.format(self.symbol,self.interval,self.outputsize,self.key)
 		pass
-		
-# string s = string.Format("Hey, {0} it is the {1}st day of {2}.  I feel {3}!", _name, _day, _month, _feeling);
 
 if __name__ == '__main__':
-	url = MyULF()
-	url.setInterval()
-	url.setOutputSize()
-	symbolList = [ "GOOGL" , "AAPL" , "CRON" , "DNR"]
+	urlBuilder = UrlBuilder()
+	urlBuilder.setInterval()
+	urlBuilder.setOutputSize()
+	symbolList = [ "TLRY" , "AAPL" , "CRON" , "DNR"]
 	for s in symbolList:
-		url.setSymbol(s)
-		data = requests.get(url = url.getURL()).json()
-		with open(s+'.txt','w') as f:
-			json.dump(data, f)
-
-	# print(json.dump(get("AAPL"),indent= 2))
+		urlBuilder.setSymbol(s)
+		data = requests.get(url = urlBuilder.getURL()).json()
+		with open('data/'+s+'.json','w') as f:
+			json.dump(data, f,indent=2)
