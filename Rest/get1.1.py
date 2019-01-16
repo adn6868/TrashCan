d98ec287a3548909326673345e805324d1c90acc
Anh Nguyen_MacbookPro <anhnd32@fsoft.com.vn>
2019-01-16 10:03:06 -0600

    StockScanner 2.0
    Multithreading, speed x100 times

--- Rest/get1.1.py
+++ Rest/get1.1.py
@@ -1,20 +1,19 @@
 import requests
 import time
 import threading
-import time
 import concurrent.futures
 
 LOCAL_THREAD = threading.local()
 
 def thread_session ():
-	if LOCAL_THREAD.session != None:
-		LOCAL_THREAD.session = requests.session()
+	if not getattr(LOCAL_THREAD, "session", None):
+		LOCAL_THREAD.session = requests.Session()
 	return LOCAL_THREAD.session
 
 def get(url):
 	session = thread_session()
 	with session.get(url) as response:
-		print("get %d from %s" % (len(response.content), url))
+		print("get {0} from {1}".format(len(response.content), url))
 
 # def getList(sites):
 # 	with requests.Session() as session:
@@ -22,7 +21,7 @@ def get(url):
 # 			get(url,session)
 
 def getList(sites):
-	with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+	with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
 		executor.map(get, sites)
 
 
--- StockScanner/main.py
+++ StockScanner/main.py
@@ -1,11 +1,19 @@
-import requests
-import time
-import threading
 import concurrent.futures
 import json
+import requests
+from sys import stdout as O
+import threading
+import time
+
+NUMBER_OF_THREAD = 5
+KEY = 'EBBVJ2TH7C8Y5HEA'
+INPUT_FILE = 'symbolList.txt'
 
 class UrlBuilder:
-	def __init__(self, key = 'EBBVJ2TH7C8Y5HEA'):
+	'''
+	Update this to match API
+	'''
+	def __init__(self, key = KEY):
 		self.key = key
 		self.symbol = ''
 		self.function = ''
@@ -29,13 +37,41 @@ class UrlBuilder:
 			.format(self.symbol,self.interval,self.outputsize,self.key)
 		pass
 
+LOCAL_THREAD = threading.local()
+
+def thread_requests():
+	if not getattr(LOCAL_THREAD,"requests",None):
+		LOCAL_THREAD.requests = requests
+	return LOCAL_THREAD.requests
+
+def thread_UrlBuilder():
+	if not getattr(LOCAL_THREAD,"urlBuilder",None):
+		LOCAL_THREAD.urlBuilder = UrlBuilder()
+	return LOCAL_THREAD.urlBuilder
+
+def download(symbol):
+	O.write("Downloading: {0} \n".format(symbol))
+	urlBuilder = thread_UrlBuilder()
+	urlBuilder.setSymbol(symbol)
+	requests = thread_requests()
+	data = requests.get(url = urlBuilder.getURL()).json()
+	O.write("Download success: {0} \n".format(symbol))
+	with open('data/'+symbol+'.json','w') as f:
+		json.dump(data, f,indent=2)
+
+def download_list(symbolList):
+	with concurrent.futures.ThreadPoolExecutor(max_workers = NUMBER_OF_THREAD) as executor:
+		executor.map(download, symbolList)
+
 if __name__ == '__main__':
 	urlBuilder = UrlBuilder()
 	urlBuilder.setInterval()
 	urlBuilder.setOutputSize()
 	symbolList = [ "TLRY" , "AAPL" , "CRON" , "DNR"]
-	for s in symbolList:
-		urlBuilder.setSymbol(s)
-		data = requests.get(url = urlBuilder.getURL()).json()
-		with open('data/'+s+'.json','w') as f:
-			json.dump(data, f,indent=2)
+	with open(INPUT_FILE,'r') as f:
+		symbolList = f.read().split('\n')
+	
+	startTime = time.time()
+	download_list(symbolList)
+	duration = time.time() - startTime
+	print("download %d symbols in %s seconds " % (len(symbolList),str(duration)) )
